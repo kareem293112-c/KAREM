16f013beb2da4fa4e39ff66c2bcbf88badb000ad
kareem293112-c <kareem293112@gmail.com>
2026-06-30 08:21:46 +0300

    Mohamed Shibani

--- src/App.tsx
+++ src/App.tsx
@@ -788,11 +788,9 @@ export default function App() {
                 : 'text-indigo-600'
             }`}
           >
-            <span>✨</span>
             <span>
               {lang === 'ar' ? 'تطوير وإشراف: محمد شيباني (Mohamed Shibani)' : 'Developed & Supervised by: Mohamed Shibani'}
             </span>
-            <span>✨</span>
           </p>
         </div>
       </footer>
